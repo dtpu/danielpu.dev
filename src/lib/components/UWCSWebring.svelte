08777baf2b51ff635d4a734b5fd52f92d38ddd34
Daniel Pu <danielpu2007@gmail.com>
2025-11-21 15:02:21 -0500

    fix: update UW CS webring link

--- src/lib/components/UWCSWebring.svelte
+++ src/lib/components/UWCSWebring.svelte
@@ -1,11 +1,11 @@
 <div style="display: flex; align-items: center; gap: 8px; filter: var(--filter-settings)">
-	<a href="https://cs.uwatering.com/https://danielpu.dev?nav=prev">←</a>
-	<a href="https://cs.uwatering.com/https://danielpu.dev" target="_blank">
+	<a href="https://cs.uwatering.com/#https://danielpu.dev?nav=prev">←</a>
+	<a href="https://cs.uwatering.com/#https://danielpu.dev" target="_blank">
 		<img
 			src="/images/uwatering.svg"
 			alt="CS Webring"
 			style="width: 24px; height: auto; opacity: 0.8;"
 		/>
 	</a>
-	<a href="https://cs.uwatering.com/https://danielpu.dev?nav=next">→</a>
+	<a href="https://cs.uwatering.com/#https://danielpu.dev?nav=next">→</a>
 </div>
