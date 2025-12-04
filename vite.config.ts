99be67173a9bc02a10d3ff369e86fedbce1c726a
Daniel Pu <danielpu2007@gmail.com>
2025-12-04 13:03:10 -0500

    build: optimize build dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	ssr: {
-		external: ['@resvg/resvg-js', 'pixel-profile', 'pixel-profile-server', 'pixel-profile-utils']
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
