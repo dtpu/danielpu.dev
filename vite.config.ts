e53548cb2be503dbdcaf4b691d085f3787456ffe
Daniel Pu <danielpu2007@gmail.com>
2025-12-04 14:29:36 -0500

    build: fix vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	ssr: {
+		noExternal: [],
+		external: ['@resvg/resvg-js']
+	},
+	optimizeDeps: {
+		exclude: ['@resvg/resvg-js']
+	}
 });
